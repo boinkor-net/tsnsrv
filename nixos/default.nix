c1c024efc0c87ce9b251cca72eeb2f69575b6cb6
Andreas Fuchs <asf@boinkor.net>
2023-08-28 14:08:12 -0400

    Refactor the submodule type for services.tsnsrv.services
    This should allow using it in multiple places.

--- nixos/default.nix
+++ nixos/default.nix
@@ -4,7 +4,112 @@
   lib,
   ...
 }: {
-  options = with lib; {
+  options = with lib; let
+    serviceSubmodule = {
+      options = {
+        authKeyPath = lib.mkOption {
+          description = "Path to a file containing a tailscale auth key. Make this a secret";
+          type = types.path;
+          default = config.services.tsnsrv.defaults.authKeyPath;
+        };
+
+        ephemeral = mkOption {
+          description = "Delete the tailnet participant shortly after it goes offline";
+          type = types.bool;
+          default = false;
+        };
+
+        funnel = mkOption {
+          description = "Serve HTTP as a funnel, meaning that it is available on the public internet.";
+          type = types.bool;
+          default = false;
+        };
+
+        insecureHTTPS = mkOption {
+          description = "Disable TLS certificate validation for requests from upstream. Insecure.";
+          type = types.bool;
+          default = false;
+        };
+
+        listenAddr = mkOption {
+          description = "Address to listen on";
+          type = types.str;
+          default = ":443";
+        };
+
+        package = mkOption {
+          description = "Package to use for this tsnsrv service.";
+          default = config.services.tsnsrv.defaults.package;
+          type = types.package;
+        };
+
+        plaintext = mkOption {
+          description = "Whether to serve non-TLS-encrypted plaintext HTTP";
+          type = types.bool;
+          default = false;
+        };
+
+        upstreamUnixAddr = mkOption {
+          description = "Connect only to the given UNIX Domain Socket";
+          type = types.nullOr types.path;
+          default = null;
+        };
+
+        prefixes = mkOption {
+          description = "URL path prefixes to allow in forwarding. Acts as an allowlist but if unset, all prefixes are allowed.";
+          type = types.listOf types.str;
+          default = [];
+        };
+
+        stripPrefix = mkOption {
+          description = "Strip matched prefix from request to upstream. Probably should be true when allowlisting multiple prefixes.";
+          type = types.bool;
+          default = true;
+        };
+
+        whoisTimeout = mkOption {
+          description = "Maximum amount of time that a requestor lookup may take.";
+          type = types.nullOr types.str;
+          default = null;
+        };
+
+        suppressWhois = mkOption {
+          description = "Disable passing requestor information to upstream service";
+          type = types.bool;
+          default = false;
+        };
+
+        upstreamHeaders = mkOption {
+          description = "Headers to set on requests to upstream.";
+          type = types.attrsOf types.str;
+          default = {};
+        };
+
+        suppressTailnetDialer = mkOption {
+          description = "Disable using the tsnet-provided dialer, which can sometimes cause issues hitting addresses outside the tailnet";
+          type = types.bool;
+          default = false;
+        };
+
+        readHeaderTimeout = mkOption {
+          description = "";
+          type = types.nullOr types.str;
+          default = null;
+        };
+
+        toURL = mkOption {
+          description = "URL to forward HTTP requests to";
+          type = types.str;
+        };
+
+        supplementalGroups = mkOption {
+          description = "List of groups to run the service under (in addition to the 'tsnsrv' group)";
+          type = types.listOf types.str;
+          default = [];
+        };
+      };
+    };
+  in {
     services.tsnsrv.enable = mkOption {
       description = "Enable tsnsrv";
       type = types.bool;
@@ -27,110 +132,7 @@
     services.tsnsrv.services = mkOption {
       description = "tsnsrv services";
       default = {};
-      type = types.attrsOf (types.submodule {
-        options = {
-          authKeyPath = lib.mkOption {
-            description = "Path to a file containing a tailscale auth key. Make this a secret";
-            type = types.path;
-            default = config.services.tsnsrv.defaults.authKeyPath;
-          };
-
-          ephemeral = mkOption {
-            description = "Delete the tailnet participant shortly after it goes offline";
-            type = types.bool;
-            default = false;
-          };
-
-          funnel = mkOption {
-            description = "Serve HTTP as a funnel, meaning that it is available on the public internet.";
-            type = types.bool;
-            default = false;
-          };
-
-          insecureHTTPS = mkOption {
-            description = "Disable TLS certificate validation for requests from upstream. Insecure.";
-            type = types.bool;
-            default = false;
-          };
-
-          listenAddr = mkOption {
-            description = "Address to listen on";
-            type = types.str;
-            default = ":443";
-          };
-
-          package = mkOption {
-            description = "Package to use for this tsnsrv service.";
-            default = config.services.tsnsrv.defaults.package;
-            type = types.package;
-          };
-
-          plaintext = mkOption {
-            description = "Whether to serve non-TLS-encrypted plaintext HTTP";
-            type = types.bool;
-            default = false;
-          };
-
-          upstreamUnixAddr = mkOption {
-            description = "Connect only to the given UNIX Domain Socket";
-            type = types.nullOr types.path;
-            default = null;
-          };
-
-          prefixes = mkOption {
-            description = "URL path prefixes to allow in forwarding. Acts as an allowlist but if unset, all prefixes are allowed.";
-            type = types.listOf types.str;
-            default = [];
-          };
-
-          stripPrefix = mkOption {
-            description = "Strip matched prefix from request to upstream. Probably should be true when allowlisting multiple prefixes.";
-            type = types.bool;
-            default = true;
-          };
-
-          whoisTimeout = mkOption {
-            description = "Maximum amount of time that a requestor lookup may take.";
-            type = types.nullOr types.str;
-            default = null;
-          };
-
-          suppressWhois = mkOption {
-            description = "Disable passing requestor information to upstream service";
-            type = types.bool;
-            default = false;
-          };
-
-          upstreamHeaders = mkOption {
-            description = "Headers to set on requests to upstream.";
-            type = types.attrsOf types.str;
-            default = {};
-          };
-
-          suppressTailnetDialer = mkOption {
-            description = "Disable using the tsnet-provided dialer, which can sometimes cause issues hitting addresses outside the tailnet";
-            type = types.bool;
-            default = false;
-          };
-
-          readHeaderTimeout = mkOption {
-            description = "";
-            type = types.nullOr types.str;
-            default = null;
-          };
-
-          toURL = mkOption {
-            description = "URL to forward HTTP requests to";
-            type = types.str;
-          };
-
-          supplementalGroups = mkOption {
-            description = "List of groups to run the service under (in addition to the 'tsnsrv' group)";
-            type = types.listOf types.str;
-            default = [];
-          };
-        };
-      });
+      type = types.attrsOf (types.submodule serviceSubmodule);
       example = false;
     };
   };
