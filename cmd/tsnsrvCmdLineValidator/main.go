f57dc335f9edd4468bb4ba38cd1ce983581134ee
Andreas Fuchs <asf@boinkor.net>
2024-02-26 20:33:25 -0500

    Buffer the signal channel on tsnsrvCmdLineValidator

--- cmd/tsnsrvCmdLineValidator/main.go
+++ cmd/tsnsrvCmdLineValidator/main.go
@@ -15,7 +15,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Invalid CLI usage. Errors:\n%v\n\n%v", errors.Unwrap(err), ffcli.DefaultUsageFunc(cmd))
 	}
-	done := make(chan os.Signal)
+	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt)
 	<-done
 }
