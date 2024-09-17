00befcb0c20a7ac50ac9625869cb36e51026f23b
Andreas Fuchs <asf@boinkor.net>
2024-09-17 19:25:29 -0400

    Reformat the dev flake-part.nix

--- dev/flake-part.nix
+++ dev/flake-part.nix
@@ -1,11 +1,21 @@
-{ self, inputs, ... }: {
+{
+  self,
+  inputs,
+  ...
+}: {
   imports = [
     inputs.devshell.flakeModule
     inputs.generate-go-sri.flakeModules.default
   ];
-  systems = [ "x86_64-linux" "aarch64-darwin" ];
+  systems = ["x86_64-linux" "aarch64-darwin"];
 
-  perSystem = {config, pkgs, system, flocken, ...}: {
+  perSystem = {
+    config,
+    pkgs,
+    system,
+    flocken,
+    ...
+  }: {
     go-sri-hashes.tsnsrv = {};
 
     devshells.default = {
@@ -35,19 +45,19 @@
             if pkgs.lib.hasSuffix "/merge" ref
             then "pr-${pkgs.lib.removeSuffix "/merge" ref}"
             else ref;
-          in {
-            inherit branch;
-            name = "ghcr.io/" + builtins.getEnv "GITHUB_REPOSITORY";
-            version = builtins.getEnv "VERSION";
+        in {
+          inherit branch;
+          name = "ghcr.io/" + builtins.getEnv "GITHUB_REPOSITORY";
+          version = builtins.getEnv "VERSION";
 
-            # Here we build the x86_64-linux variants only because
-            # that is what runs on GHA, whence we push the images to
-            # ghcr.
-            images = with self.packages; [
-              x86_64-linux.tsnsrvOciImage
-              x86_64-linux.tsnsrvOciImage-cross-aarch64-linux
-            ];
-          });
+          # Here we build the x86_64-linux variants only because
+          # that is what runs on GHA, whence we push the images to
+          # ghcr.
+          images = with self.packages; [
+            x86_64-linux.tsnsrvOciImage
+            x86_64-linux.tsnsrvOciImage-cross-aarch64-linux
+          ];
+        });
         type = "app";
       };
     };
