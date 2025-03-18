7e92020b85d0da521a49284e3cc4b2fee51d93b1
Andreas Fuchs <asf@boinkor.net>
2025-03-18 11:48:14 -0400

    Fold oci-urlparts into oci.nix

--- nixos/tests/e2e/default.nix
+++ nixos/tests/e2e/default.nix
@@ -6,5 +6,4 @@
 }: {
   systemd = import ./systemd.nix {inherit pkgs nixos-lib nixosModule;};
   oci = import ./oci.nix {inherit pkgs nixos-lib nixosModule;};
-  oci-urlparts = import ./oci-urlparts.nix {inherit pkgs nixos-lib nixosModule;};
 }
