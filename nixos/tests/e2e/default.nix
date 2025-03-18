2e6141951a32460d2c47b327ccd0d4e85582254e
Andreas Fuchs <asf@boinkor.net>
2025-03-18 10:09:18 -0400

    Fix e2e/systemd* tests
    There are some race conditions, especially in situations where many e2e tests are running simultaneously: By using the same ConditionPathExists trick that oci tests use, we can straighten out the sequence of events that lead to the tsnsrv service running in the systemd tests, too.
    
    Also, fold the things that systemd-urlparts.nix tests into systemd.nix, this should free up some runner resources and give us a faster test.

--- nixos/tests/e2e/default.nix
+++ nixos/tests/e2e/default.nix
@@ -5,7 +5,6 @@
   ...
 }: {
   systemd = import ./systemd.nix {inherit pkgs nixos-lib nixosModule;};
-  systemd-urlparts = import ./systemd-urlparts.nix {inherit pkgs nixos-lib nixosModule;};
   oci = import ./oci.nix {inherit pkgs nixos-lib nixosModule;};
   oci-urlparts = import ./oci-urlparts.nix {inherit pkgs nixos-lib nixosModule;};
 }
