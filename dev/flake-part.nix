bacba71592ff66f817fee51ed6ec67b4229ee204
Andreas Fuchs <asf@boinkor.net>
2025-08-25 08:43:57 -0400

    Update nix build to use go 1.24

--- dev/flake-part.nix
+++ dev/flake-part.nix
@@ -2,72 +2,78 @@
   self,
   inputs,
   ...
-}: {
+}:
+{
   imports = [
     inputs.devshell.flakeModule
     inputs.generate-go-sri.flakeModules.default
   ];
-  systems = ["x86_64-linux" "aarch64-darwin"];
+  systems = [
+    "x86_64-linux"
+    "aarch64-darwin"
+  ];
 
-  perSystem = {
-    config,
-    pkgs,
-    system,
-    flocken,
-    ...
-  }: {
-    go-sri-hashes.tsnsrv = {};
+  perSystem =
+    {
+      config,
+      pkgs,
+      system,
+      flocken,
+      ...
+    }:
+    {
+      go-sri-hashes.tsnsrv = { };
 
-    devshells.default = {
-      commands = [
-        {
-          name = "regenSRI";
-          category = "dev";
-          help = "Regenerate tsnsrv.sri in case the module SRI hash should change";
-          command = "${config.apps.generate-sri-tsnsrv.program}";
-        }
-      ];
-      packages = [
-        pkgs.go_1_23
-        pkgs.gopls
-        pkgs.golangci-lint
-      ];
-    };
+      devshells.default = {
+        commands = [
+          {
+            name = "regenSRI";
+            category = "dev";
+            help = "Regenerate tsnsrv.sri in case the module SRI hash should change";
+            command = "${config.apps.generate-sri-tsnsrv.program}";
+          }
+        ];
+        packages = [
+          pkgs.go_1_24
+          pkgs.gopls
+          pkgs.golangci-lint
+        ];
+      };
 
-    apps = {
-      default = config.apps.tsnsrv;
-      tsnsrv.program = config.packages.tsnsrv;
+      apps = {
+        default = config.apps.tsnsrv;
+        tsnsrv.program = config.packages.tsnsrv;
 
-      pushImagesToGhcr = {
-        program = inputs.flocken.legacyPackages.${system}.mkDockerManifest (let
-          ref = builtins.getEnv "GITHUB_REF_NAME";
-          isPR = pkgs.lib.hasSuffix "/merge" ref;
-          branch =
-            if isPR
-            then "pr-${pkgs.lib.removeSuffix "/merge" ref}"
-            else ref;
-        in {
-          autoTags = {
-            branch = true;
-            version = true;
-          };
-          inherit branch;
+        pushImagesToGhcr = {
+          program = inputs.flocken.legacyPackages.${system}.mkDockerManifest (
+            let
+              ref = builtins.getEnv "GITHUB_REF_NAME";
+              isPR = pkgs.lib.hasSuffix "/merge" ref;
+              branch = if isPR then "pr-${pkgs.lib.removeSuffix "/merge" ref}" else ref;
+            in
+            {
+              autoTags = {
+                branch = true;
+                version = true;
+              };
+              inherit branch;
 
-          github = {
-            enable = true;
-            token = "$GH_TOKEN";
-          };
+              github = {
+                enable = true;
+                token = "$GH_TOKEN";
+              };
 
-          # Here we build the x86_64-linux variants only because
-          # that is what runs on GHA, whence we push the images to
-          # ghcr.
-          images = with self.packages; [
-            x86_64-linux.tsnsrvOciImage
-            x86_64-linux.tsnsrvOciImage-cross-aarch64-linux
-          ];
-        });
-        type = "app";
+              # Here we build the x86_64-linux variants only because
+              # that is what runs on GHA, whence we push the images to
+              # ghcr.
+              images = with self.packages; [
+                x86_64-linux.tsnsrvOciImage
+                x86_64-linux.tsnsrvOciImage-cross-aarch64-linux
+              ];
+            }
+          );
+          type = "app";
+        };
       };
     };
-  };
 }
--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
-  outputs = inputs @ {
-    self,
-    flake-parts,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
         inputs.flake-parts.flakeModules.partitions
@@ -16,55 +17,71 @@
         "aarch64-darwin"
         "aarch64-linux"
       ];
-      perSystem = {
-        config,
-        lib,
-        pkgs,
-        system,
-        ...
-      }: let
-        tsnsrvPkg = p: subPackage:
-          p.buildGo123Module {
-            pname = builtins.baseNameOf subPackage;
-            version = "0.0.0";
-            vendorHash = builtins.readFile ./tsnsrv.sri;
-            src = lib.sourceFilesBySuffices (lib.sources.cleanSource ./.) [".go" ".mod" ".sum"];
-            subPackages = [subPackage];
-            ldflags = ["-s" "-w"];
-            meta.mainProgram = builtins.baseNameOf subPackage;
+      perSystem =
+        {
+          config,
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          tsnsrvPkg =
+            p: subPackage:
+            p.buildGo124Module {
+              pname = builtins.baseNameOf subPackage;
+              version = "0.0.0";
+              vendorHash = builtins.readFile ./tsnsrv.sri;
+              src = lib.sourceFilesBySuffices (lib.sources.cleanSource ./.) [
+                ".go"
+                ".mod"
+                ".sum"
+              ];
+              subPackages = [ subPackage ];
+              ldflags = [
+                "-s"
+                "-w"
+              ];
+              meta.mainProgram = builtins.baseNameOf subPackage;
+            };
+          imageArgs = p: {
+            name = "tsnsrv";
+            tag = "latest";
+            contents = [
+              (p.buildEnv {
+                name = "image-root";
+                paths = [ (tsnsrvPkg p "cmd/tsnsrv") ];
+                pathsToLink = [
+                  "/bin"
+                  "/tmp"
+                ];
+              })
+              p.dockerTools.caCertificates
+            ];
+
+            config.EntryPoint = [ "/bin/tsnsrv" ];
           };
-        imageArgs = p: {
-          name = "tsnsrv";
-          tag = "latest";
-          contents = [
-            (p.buildEnv {
-              name = "image-root";
-              paths = [(tsnsrvPkg p "cmd/tsnsrv")];
-              pathsToLink = ["/bin" "/tmp"];
-            })
-            p.dockerTools.caCertificates
-          ];
+        in
+        {
+          overlayAttrs = {
+            inherit (config.packages) tsnsrv tsnsrvOciImage;
+          };
+          packages = {
+            default = config.packages.tsnsrv;
+            tsnsrv = tsnsrvPkg pkgs "cmd/tsnsrv";
+            tsnsrvCmdLineValidator = tsnsrvPkg pkgs "cmd/tsnsrvCmdLineValidator";
 
-          config.EntryPoint = ["/bin/tsnsrv"];
+            # This platform's "natively" built docker image:
+            tsnsrvOciImage = pkgs.dockerTools.buildLayeredImage (imageArgs pkgs);
+
+            # "cross-platform" build, mainly to support building on github actions (but also on macOS with apple silicon):
+            tsnsrvOciImage-cross-aarch64-linux = pkgs.pkgsCross.aarch64-multiplatform.dockerTools.buildLayeredImage (
+              imageArgs pkgs.pkgsCross.aarch64-multiplatform
+            );
+          };
+
+          formatter = pkgs.alejandra;
         };
-      in {
-        overlayAttrs = {
-          inherit (config.packages) tsnsrv tsnsrvOciImage;
-        };
-        packages = {
-          default = config.packages.tsnsrv;
-          tsnsrv = tsnsrvPkg pkgs "cmd/tsnsrv";
-          tsnsrvCmdLineValidator = tsnsrvPkg pkgs "cmd/tsnsrvCmdLineValidator";
-
-          # This platform's "natively" built docker image:
-          tsnsrvOciImage = pkgs.dockerTools.buildLayeredImage (imageArgs pkgs);
-
-          # "cross-platform" build, mainly to support building on github actions (but also on macOS with apple silicon):
-          tsnsrvOciImage-cross-aarch64-linux = pkgs.pkgsCross.aarch64-multiplatform.dockerTools.buildLayeredImage (imageArgs pkgs.pkgsCross.aarch64-multiplatform);
-        };
-
-        formatter = pkgs.alejandra;
-      };
 
       partitionedAttrs = {
         checks = "dev";
@@ -77,7 +94,7 @@
       };
       flake = {
         nixosModules = {
-          default = import ./nixos {flake = self;};
+          default = import ./nixos { flake = self; };
         };
       };
     };
